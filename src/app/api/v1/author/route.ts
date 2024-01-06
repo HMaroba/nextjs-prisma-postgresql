0779ec61dad1c1251992c7a39a3b6b75f37ef00f
Coffee58511867 <marobahlalele910@gmail.com>
2024-01-06 05:40:26 +0200

    Created author post api

---
+++ src/app/api/v1/author/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function POST(req: NextRequest) {
+  try {
+    const { name, email } = await req.json();
+
+    const isAuthorExisting = await prisma.author.findFirst({
+      where: { email },
+    });
+
+    if (isAuthorExisting) {
+      return NextResponse.json(
+        { success: false, message: "Author already exists!" },
+        { status: 422 }
+      );
+    }
+
+    await prisma.author.create({
+      data: { name, email },
+    });
+
+    return NextResponse.json(
+      { success: true, message: "Author created successfully" },
+      { status: 201 }
+    );
+  } catch (error) {
+    return new NextResponse("Something went wrong" + error, { status: 500 });
+  }
+}
